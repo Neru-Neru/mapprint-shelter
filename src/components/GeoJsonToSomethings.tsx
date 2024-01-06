aad1f021da9f46f6dec5f251a7dd1284e9c4ece7
Neru-neru <yumadachi0113soccer@gmail.com>
2024-01-06 16:49:30 +0900

    クラスタ表示のみ実装

--- src/components/GeoJsonToSomethings.tsx
+++ src/components/GeoJsonToSomethings.tsx
@@ -13,279 +13,144 @@ export const GeoJsonToSomethings: React.FC<{
     emoji?: string;
   };
 }> = ({ geojson, emoji, style }) => {
-  const { current: map } = useMap();
+  // const { current: map } = useMap();
 
-  const [currentZoom, setCurrentZoom] = useState<number | undefined>(8);
-  const [opacity, setOpacity] = useState(0.8);
-  const [fontSize, setFontSize] = useState("1em");
+  // const [currentZoom, setCurrentZoom] = useState<number | undefined>(8);
+  // const [opacity, setOpacity] = useState(0.8);
+  // const [fontSize, setFontSize] = useState("1em");
 
-  useEffect(() => {
-    if (!map) return;
-    map.on("load", () => {
-      setCurrentZoom(map.getZoom());
-    });
-    map.on("move", () => {
-      setCurrentZoom(map.getZoom());
-    });
-    setCurrentZoom(map.getZoom());
-  }, [map]);
+  // useEffect(() => {
+  //   if (!map) return;
+  //   map.on("load", () => {
+  //     setCurrentZoom(map.getZoom());
+  //   });
+  //   map.on("move", () => {
+  //     setCurrentZoom(map.getZoom());
+  //   });
+  //   setCurrentZoom(map.getZoom());
+  // }, [map]);
 
-  // determine style of markers
-  useEffect(() => {
-    let newOpacity = 0.8;
-    let newFontSize = "1em";
-    if (currentZoom) {
-      if (14 <= currentZoom) {
-        newOpacity = 1;
-        newFontSize = "1.4em";
-      }
-      if (currentZoom < 14) {
-        newOpacity = 0.8;
-        newFontSize = "1.4em";
-      }
-      if (currentZoom < 13) {
-        newOpacity = 0.75;
-        newFontSize = "1.3em";
-      }
-      if (currentZoom < 12) {
-        newOpacity = 0.7;
-        newFontSize = "1.1em";
-      }
-      if (currentZoom < 11) {
-        newOpacity = 0.6;
-        newFontSize = "1em";
-      }
-      if (currentZoom < 10) {
-        newOpacity = 0.65;
-        newFontSize = "1em";
-      }
-    }
-    //console.info("zoom, size, opacity:", currentZoom, newFontSize, newOpacity);
-    setOpacity(newOpacity);
-    setFontSize(newFontSize);
-  }, [currentZoom]);
+  // // determine style of markers
+  // useEffect(() => {
+  //   let newOpacity = 0.8;
+  //   let newFontSize = "1em";
+  //   if (currentZoom) {
+  //     if (14 <= currentZoom) {
+  //       newOpacity = 1;
+  //       newFontSize = "1.4em";
+  //     }
+  //     if (currentZoom < 14) {
+  //       newOpacity = 0.8;
+  //       newFontSize = "1.4em";
+  //     }
+  //     if (currentZoom < 13) {
+  //       newOpacity = 0.75;
+  //       newFontSize = "1.3em";
+  //     }
+  //     if (currentZoom < 12) {
+  //       newOpacity = 0.7;
+  //       newFontSize = "1.1em";
+  //     }
+  //     if (currentZoom < 11) {
+  //       newOpacity = 0.6;
+  //       newFontSize = "1em";
+  //     }
+  //     if (currentZoom < 10) {
+  //       newOpacity = 0.65;
+  //       newFontSize = "1em";
+  //     }
+  //   }
+  //   //console.info("zoom, size, opacity:", currentZoom, newFontSize, newOpacity);
+  //   setOpacity(newOpacity);
+  //   setFontSize(newFontSize);
+  // }, [currentZoom]);
 
-  const onClickMarker = useCallback(
-    (center: Feature<Point, GeoJsonProperties> | undefined) => {
-      if (map === undefined || center === undefined) {
-        return;
-      }
-      const zoomTo = map.getZoom() < 10 ? 10 : 14;
-      map.flyTo({
-        center: [
-          center.geometry.coordinates[0],
-          center.geometry.coordinates[1],
-        ],
-        zoom: zoomTo,
-      });
-    },
-    [map]
-  );
+  // const onClickMarker = useCallback(
+  //   (center: Feature<Point, GeoJsonProperties> | undefined) => {
+  //     if (map === undefined || center === undefined) {
+  //       return;
+  //     }
+  //     const zoomTo = map.getZoom() < 10 ? 10 : 14;
+  //     map.flyTo({
+  //       center: [
+  //         center.geometry.coordinates[0],
+  //         center.geometry.coordinates[1],
+  //       ],
+  //       zoom: zoomTo,
+  //     });
+  //   },
+  //   [map]
+  // );
 
   if (geojson === undefined || geojson.features === undefined) {
     return null;
   }
 
   return (
-    <>
-      {geojson.features.map((feature, index) => {
-        if (feature.geometry === undefined) {
-          return null;
-        }
-        if (
-          feature.geometry.type !== "Polygon" &&
-          feature.geometry.type !== "MultiPolygon" &&
-          feature.geometry.type !== "LineString" &&
-          feature.geometry.type !== "Point"
-        ) {
-          return null;
-        }
+    <Source
+      id="clusterSource"
+      type="geojson"
+      data={geojson}
+      cluster={true}
+      clusterMaxZoom={14}
+      clusterRadius={50}
+    >
+      <Layer
+        {...{
+          id: "clusters",
+          type: "circle",
+          source: "clusterSource",
+          filter: ["has", "point_count"],
+          paint: {
+            "circle-color": [
+              "step",
+              ["get", "point_count"],
+              "#51bbd6",
+              100,
+              "#f1f075",
+              750,
+              "#f28cb1",
+            ],
+            "circle-radius": [
+              "step",
+              ["get", "point_count"],
+              20,
+              100,
+              30,
+              750,
+              40,
+            ],
+          },
+        }}
+      />
 
-        let zIndex = 100;
-        let icon = emoji;
-        let title = "No name";
+      <Layer
+        {...{
+          id: "cluster-count",
+          type: "symbol",
+          source: "clusterSource",
+          filter: ["has", "point_count"],
+          layout: {
+            "text-field": "{point_count_abbreviated}",
+            "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          },
+        }}
+      />
 
-        if (
-          feature.properties &&
-          feature.properties.name &&
-          feature.properties.name.length > 0
-        ) {
-          title = feature.properties.name;
-        }
-
-        if (style?.emoji) {
-          icon = style.emoji;
-        }
-        // United Nations
-        if (
-          (feature.properties &&
-            feature.properties.operator &&
-            (feature.properties.operator.includes("UN") ||
-              feature.properties.operator.includes("United Nations"))) ||
-          (feature.properties &&
-            feature.properties.name &&
-            (feature.properties.name.includes("UN") ||
-              feature.properties.name.includes("United Nations")))
-        ) {
-          icon = "🇺🇳";
-          zIndex = 110;
-        }
-
-        let center: Feature<Point, GeoJsonProperties> | undefined = undefined;
-        switch (feature.geometry.type) {
-          case "Polygon":
-            const polygonFeatures = turf.polygon(feature.geometry.coordinates);
-            center = turf.centroid(polygonFeatures);
-            break;
-          case "MultiPolygon":
-            const multiPolygonFeatures = turf.multiPolygon(
-              feature.geometry.coordinates
-            );
-            center = turf.centroid(multiPolygonFeatures);
-            break;
-          case "LineString":
-            const bbox = turf.bbox(feature);
-            const polygon = turf.bboxPolygon(bbox);
-            center = turf.centroid(polygon);
-            break;
-          case "Point":
-            center = turf.point(feature.geometry.coordinates);
-          default:
-            break;
-        }
-        if (center === undefined) {
-          return null;
-        }
-
-        return (
-          <Fragment key={`${feature.id}-${index}`}>
-            {feature.geometry.type === "LineString" && (
-              <Source type="geojson" data={feature}>
-                {
-                  <Layer
-                    {...{
-                      id: `${feature.id}-line`,
-                      type: "line",
-                      paint: {
-                        "line-width": 4,
-                        "line-color": style?.color ? style.color : "#f2f8fc",
-                        "line-opacity": 0.8,
-                      },
-                    }}
-                  />
-                }
-              </Source>
-            )}
-            {(feature.geometry.type === "Polygon" ||
-              feature.geometry.type === "MultiPolygon") && (
-              <Source type="geojson" data={feature}>
-                {
-                  <Layer
-                    {...{
-                      id: `${feature.id}-line`,
-                      type: "line",
-                      paint: {
-                        "line-width": 4,
-                        "line-color": style?.color ? style.color : "#f2f8fc",
-                        "line-opacity": 0.8,
-                      },
-                    }}
-                  />
-                }
-                {
-                  <Layer
-                    {...{
-                      id: `${feature.id}-fill`,
-                      type: "fill",
-                      paint: {
-                        "fill-color": style?.fillColor
-                          ? style.fillColor
-                          : style?.color
-                          ? style.color
-                          : "#00b8ff",
-                        "fill-opacity": 0.2,
-                      },
-                    }}
-                  />
-                }
-              </Source>
-            )}
-            {icon && (
-              <Marker
-                key={feature.id}
-                longitude={center.geometry.coordinates[0]}
-                latitude={center.geometry.coordinates[1]}
-                onClick={() => onClickMarker(center)}
-                style={{ zIndex: zIndex }}
-              >
-                <div
-                  title={title}
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    cursor: "pointer",
-                    opacity: opacity,
-                    lineHeight: "1",
-                    textAlign: "center",
-                    width: "30px",
-                    height: "30px",
-                    border: "solid 2px",
-                    borderRadius: "0 70% 70%",
-                    boxShadow: "0 0 2px #000",
-                    transformOrigin: "0 0",
-                    transform: "rotateZ(-135deg)",
-                    overflow: "hidden",
-                    backgroundColor: style?.fillColor
-                      ? style.fillColor
-                      : "rgba(255, 255, 255, 0.7)",
-                    backdropFilter: "blur(4px)",
-                    boxSizing: "border-box",
-                    WebkitPrintColorAdjust: "exact",
-                  }}
-                >
-                  <div
-                    style={{
-                      transform: "rotateZ(135deg)",
-                      fontSize: fontSize,
-                      fontFamily: "sans-serif, emoji",
-                      lineHeight: "1.1",
-                      WebkitPrintColorAdjust: "exact",
-                      position: "relative",
-                    }}
-                  >
-                    <span
-                      style={{
-                        fontSize: "1.2em",
-                        fontFamily: "sans-serif",
-                        zIndex: 100,
-                        color: style?.color
-                          ? style.color
-                          : "rgba(0, 0, 0, 0.8)",
-                      }}
-                    >
-                      {index + 1}
-                    </span>
-                  </div>
-                </div>{" "}
-                <span
-                  style={{
-                    fontSize: "1.2em",
-                    fontFamily: "sans-serif, emoji",
-                    position: "absolute",
-                    bottom: "0.6em",
-                    left: "-0.6em",
-                    zIndex: 10,
-                  }}
-                >
-                  {icon}
-                </span>
-              </Marker>
-            )}
-          </Fragment>
-        );
-      })}
-    </>
+      <Layer
+        {...{
+          id: "unclustered-point",
+          type: "circle",
+          source: "clusterSource",
+          filter: ["!", ["has", "point_count"]],
+          paint: {
+            "circle-color": "#11b4da",
+            "circle-radius": 10,
+            "circle-stroke-width": 1,
+            "circle-stroke-color": "#fff",
+          },
+        }}
+      />
+    </Source>
   );
 };
